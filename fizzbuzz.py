961d4eead30987f968d0eb3150712332020f4f80
sososo <sh0000@pusan.ac.kr>
2023-05-01 16:38:45 +0900

    feat: Python env test
    Create do_fizzbuzz and test.

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def do_fizzbuzz():
+    """
+    fizzbuzz: print fizz buzz fizzbuzz
+    3: fizz
+    5: buzz
+    15: fizzbuzz
+    etc: num
+    """
+    print("hello")
+
+
+if __name__ == '__main__':
+    do_fizzbuzz()
