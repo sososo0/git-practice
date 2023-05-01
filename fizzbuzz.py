8d94a97fb92072ded1f5cbb83f1e6e6678c1d564
sososo <sh0000@pusan.ac.kr>
2023-05-01 16:48:01 +0900

    feat: Print buzz
    Condition: i is multiple of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,6 +9,8 @@ def do_fizzbuzz(num: int):
     for i in range(1, num+1):
         if i%3==0:
             print('fizz')
+        elif i%5==0:
+            print('buzz')
         else:
             print(f'{i}')
 
